6c1457682686501f4e99be39aed2ef7fb11bb930
RaufHossain <raufhossain2010@yahoo.com>
2022-10-11 10:16:23 -0600

    New files

---
+++ comments.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+<?php
+    
+    /* This function below displays text */
+    echo "Hello Student";
+
+
+
+
+
+?>
---
+++ dynamic_text.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+  
+<!--  Dynamic Data-->
+   
+   <?php  
+    
+    $title = "Dynamic data";
+    
+    ?>
+    
+   <h1><?php echo $title; ?></h1>
+   
+<!--  Dynamic Data ends-->
+    
+</body>
+</html>
